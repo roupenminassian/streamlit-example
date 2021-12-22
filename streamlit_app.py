fdc69a483f859bb07a5927fd62668871f0736129
Roupen Minassian <61346944+roupenminassian@users.noreply.github.com>
2021-12-23 09:12:38 +1100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,8 @@ import altair as alt
 import math
 import pandas as pd
 import streamlit as st
+import pickle
+from rank_bm25 import BM25Okapi
 
 """
 # Welcome to Streamlit!
@@ -12,24 +14,23 @@ Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
 In the meantime, below is an example of what you can do with just a few lines of code:
 """
 
+#Load documents
+uploaded_file = st.file_uploader("https://github.com/roupenminassian/streamlit-example/test.txt")
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+with open("/content/drive/MyDrive/test.txt","rb") as fp:# Unpickling
+  contents = pickle.load(fp)
 
-    Point = namedtuple('Point', 'x y')
-    data = []
+#Preparing model
+tokenized_corpus = [doc.split(" ") for doc in contents]
 
-    points_per_turn = total_points / num_turns
+bm25 = BM25Okapi(tokenized_corpus)
 
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
+user_input = st.text_input('Seed Text (can leave blank)')
 
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+tokenized_query = query.split(" ")
+
+doc_scores = bm25.get_scores(tokenized_query)
+
+if st.button('Generate Text'):
+    generated_test = bm25.get_top_n(tokenized_query, contents, n=1)
+     st.write(generated_text)
